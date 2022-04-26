4c1ac80a56ad444a437b4fd60e58195971402e5c
RomanH83 <romanhrobotRH@gmail.com>
2022-04-26 12:58:52 +0200

    Move form related classes to forms.py

--- carrent/carrentapp/models.py
+++ carrent/carrentapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
+
 
 class AccountManagerCustom(BaseUserManager):
 
