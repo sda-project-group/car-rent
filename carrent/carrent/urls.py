3cf6885a1cf96657397b3287d1c21c44e523eddc
Ruckov <uzytkownik@kursgit.pl>
2022-05-17 14:59:44 +0200

    register accounts urls in main urls.py

--- carrent/carrent/urls.py
+++ carrent/carrent/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('carrent/', include('carrentapp.urls')),
+    path('account/', include('accounts.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
