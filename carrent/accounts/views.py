7d57e13d8c1a5c3040e7bc35fd605146cca9fe23
Ruckov <uzytkownik@kursgit.pl>
2022-05-17 14:52:35 +0200

    move user related views to accounts

--- carrent/accounts/views.py
+++ carrent/accounts/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views import View
 
-# Create your views here.
+from .forms import LoginForm, RegistrationForm, UpdateUserForm
+
+
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+
+    def get_success_url(self):
+        return reverse('main')
+
+
+class RegisterView(View):
+    form_class = RegistrationForm
+    initial = {'key': 'value'}
+    template_name = 'carrentapp/registration.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect(to='main')
+
+        return render(request, self.template_name, {'form': form})
+
+
+class UpdateProfileUserView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user_form = UpdateUserForm(instance=request.user)
+        return render(request, 'carrentapp/profile.html', {'user_form': user_form})
+
+    def post(self, request, *args, **kwargs):
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        print(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'Twój profil został pomyślnie zaktualizowany')
+            return redirect(to='user-profile')
+
+        return HttpResponse(request.POST)
+
+
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'carrentapp/password_change.html'
+    success_message = "Hasło zostało zmienione"
+    success_url = reverse_lazy('main')
+
--- carrent/carrentapp/views.py
+++ carrent/carrentapp/views.py
@@ -1,71 +1,17 @@
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import PasswordChangeView
-from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy, reverse
-from django.views import View
-from django.views.generic import CreateView, DeleteView, ListView
 import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from .forms import LoginForm, RegistrationForm, UpdateUserForm, OrderDatePickForm
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import CreateView, DeleteView, ListView
+
+from .forms import OrderDatePickForm
 from .models import BasePrice, Car, Order
 from .validators import order_date_validator, if_found_in_db
 
 
 def base_test_view(request):
     return render(request, 'carrentapp/base.html')
-
-
-class CustomLoginView(LoginView):
-    form_class = LoginForm
-
-    def get_success_url(self):
-        return reverse('main')
-
-
-class RegisterView(View):
-    form_class = RegistrationForm
-    initial = {'key': 'value'}
-    template_name = 'carrentapp/registration.html'
-
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-
-        if form.is_valid():
-            form.save()
-
-            return redirect(to='main')
-
-        return render(request, self.template_name, {'form': form})
-
-
-class UpdateProfileUserView(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs):
-        user_form = UpdateUserForm(instance=request.user)
-        return render(request, 'carrentapp/profile.html', {'user_form': user_form})
-
-    def post(self, request, *args, **kwargs):
-        user_form = UpdateUserForm(request.POST, instance=request.user)
-        print(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-            messages.success(request, 'Twój profil został pomyślnie zaktualizowany')
-            return redirect(to='user-profile')
-
-        return HttpResponse(request.POST)
-
-
-class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
-    template_name = 'carrentapp/password_change.html'
-    success_message = "Hasło zostało zmienione"
-    success_url = reverse_lazy('main')
 
 
 class CreateOrderView(CreateView):
