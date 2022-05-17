2ff2f5e3f557797c9c6ff9c24840c3e95b679f99
Ruckov <uzytkownik@kursgit.pl>
2022-05-17 14:58:19 +0200

    move user related urls to accounts

--- carrent/carrentapp/urls.py
+++ carrent/carrentapp/urls.py
@@ -4,22 +4,14 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.views.generic import ListView, DetailView, TemplateView
 
-from .forms import LoginForm
-from .views import CustomLoginView, RegisterView, ChangePasswordView, base_test_view, CreateOrderView, \
-    OrderConfirmView, UpdateProfileUserView, FutureOrderView, HistoryOrderView, ActualOrderView
+
+from .views import base_test_view, CreateOrderView, OrderConfirmView, FutureOrderView, HistoryOrderView, ActualOrderView
 from .models import Car
-from . import views
 
 urlpatterns = [
     path('base-test/', base_test_view, name='zalogowany'),
-    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='carrentapp/login.html',
-                                           authentication_form=LoginForm), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='carrentapp/logout.html'), name='logout'),
-    path('registration/', RegisterView.as_view(), name='users-registration'),
     path('carlist/', ListView.as_view(model=Car), name='car_list'),
     path('cardetail/<int:pk>/', DetailView.as_view(model=Car), name="car_detail"),
-    path('profile/', UpdateProfileUserView.as_view(), name='user-profile'),
-    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
     path('', TemplateView.as_view(template_name='carrentapp/home_page.html'), name='main'),
     path('contact', TemplateView.as_view(template_name='carrentapp/contact.html'), name='contact'),
     path('aboutus', TemplateView.as_view(template_name='carrentapp/about.html'), name='about'),
